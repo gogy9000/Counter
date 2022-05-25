4b924762513e842b9608f92677c616ad2d7a7c64
gogy9000 <goropashnyj9000@gmail.com>
2022-05-25 12:18:57 +0500

    use redux and react-redux

---
+++ src/redux/CountReducer.ts
@@ -0,0 +1,7 @@
+
+
+export const CountReducer=(state:any,action:any)=>{
+    switch (action.type) {
+        default: return state
+    }
+}
---
+++ src/redux/Store.ts
@@ -0,0 +1,5 @@
+import {createStore} from "redux";
+import {CountReducer} from "./CountReducer";
+
+
+const store= createStore(CountReducer)
